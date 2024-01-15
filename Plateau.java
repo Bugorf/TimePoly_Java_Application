1852c4d6258b6a547dfa835837cf39987fa7cc61
bonjo <bonjo@bonjo.fr>
2024-01-15 03:48:21 +0100

    Initial commit

---
+++ Plateau.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Plateau extends JFrame {
+
+    public Plateau() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1400, 1000);
+        setResizable(false);
+        setVisible(true);
+
+        GridBagLayout bagLayout = new GridBagLayout();
+        setLayout(bagLayout);
+
+        for (int i = 0; i < 11; i++) {
+            add(createLabel(String.valueOf(i)),constraints(i,0,1,1,1,1));
+        }
+        for (int i = 1; i < 10; i++) {
+            add(createLabel(String.valueOf(i)),constraints(0,i,1,1,1,1));
+        }
+
+        for (int i = 1; i < 10; i++) {
+            add(createLabel(String.valueOf(i)),constraints(10,i,1,1,1,1));
+        }
+        for (int i = 0; i < 11; i++) {
+            add(createLabel(String.valueOf(i)),constraints(i,10,1,1,1,1));
+        }
+        add(createLabel(String.valueOf("chance")),constraints(4,1,3,2,0,0));
+        add(createLabel(String.valueOf("TimePoly")),constraints(1,3,9,5,0,0));
+        add(createLabel(String.valueOf("evenement")),constraints(4,8,3,2,0,0));
+
+
+    }
+
+    public GridBagConstraints constraints(int gridx, int gridy, int gridwidth, int gridheight, int weightx, int weighty) {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = gridx;
+        constraints.gridy = gridy;
+        constraints.gridwidth = gridwidth;
+        constraints.gridheight = gridheight;
+        constraints.weightx = weightx;
+        constraints.weighty = weighty;
+
+        return constraints;
+    }
+
+    public JLabel createLabel(String texte) {
+        JLabel label = new JLabel(texte);
+        label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        return label;
+    }
+
+    public static void main(String[] args) {
+        new Plateau();
+    }
+}
